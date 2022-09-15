c4501645bef74273a17805f5b02b2072b01ed2d6
Enlighten <1292720539@qq.com>
2022-09-15 19:54:34 +0800

    IDEA  Test  5 (master b)

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -1,6 +1,10 @@
 public class gitTest {
 
     public static void main(String[] args) {
+
+
+        System.out.println("This is master Branch");
+
         System.out.println("Hello IDEA Git");
     }
 }
