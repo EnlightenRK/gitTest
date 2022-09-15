d0341099e4978d24f063ea40f7f60bbafb4e8835
Enlighten <1292720539@qq.com>
2022-09-15 19:49:39 +0800

    IDEA  Test  5 (NEW b)

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -1,6 +1,11 @@
 public class gitTest {
 
     public static void main(String[] args) {
+
+        System.out.println("This is New Branch");
         System.out.println("Hello IDEA Git");
     }
+
+
+
 }
