d85426ea30978090f6a70213b5c918097913725c
Enlighten <1292720539@qq.com>
2022-09-16 08:11:04 +0800

    IDEA  Test  6

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -1,12 +1,8 @@
 public class gitTest {
 
     public static void main(String[] args) {
-
-        System.out.println("This is New Branch");
+        System.out.println("This is New and master content Branch");
         System.out.println("Hello IDEA Git");
     }
-//d0341099e4978d24f063ea40f7f60bbafb4e8835
-
 
 }
-//d0341099e4978d24f063ea40f7f60bbafb4e8835
